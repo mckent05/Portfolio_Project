59951f55381c05b15b8e9fd851d503b1eec933f8
mckent05 <atemitope95@gmail.com>
2021-10-02 21:42:49 +0100

    Add mobile-menu

--- mobile.js
+++ mobile.js
@@ -1,17 +1,17 @@
 let btn=document.querySelector ('.icon img')
-let overlay = document.querySelector('.item-links')
+let overlay = document.querySelector('.wrapper')
 let clos= document.querySelector('.close-btn')
 
 btn.addEventListener('click', function () {
-    overlay.classList.add ('show-link')
+    overlay.classList.add ('show-nav')
 })
 clos.addEventListener ('click', function() {
-    overlay.classList.remove ('show-link')
+    overlay.classList.remove ('show-nav')
 })
 
 window.addEventListener ('scroll', function () {
     let scroll=this.pageYOffset
     if (scroll > 700) {
-        overlay.classList.remove('show-link')
+        overlay.classList.remove('show-nav')
     }
 })
